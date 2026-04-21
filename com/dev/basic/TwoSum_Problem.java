da6f599bfb6a50775dcba9b7eda87247c6f8a10f
devanya20220806-1 <devanya20220806@gmail.com>
2026-04-21 20:50:07 +0530

    added 2 sum complete code

--- com/dev/basic/TwoSum_Problem.java
+++ com/dev/basic/TwoSum_Problem.java
@@ -1,4 +1,38 @@
 
 package  com.dev.basic;
+
+import java.util.HashMap;
+
 public class TwoSum_Problem {
+
+
+    public static void main(String[] args) {
+
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = twoSum(nums, target);
+
+        if (result != null) {
+            System.out.println("Indices: " + result[0] + ", " + result[1]);
+        } else {
+            System.out.println("No two numbers found that add up to the target.");
+        }
+    }
+
+    private static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+
+
+
+            map.put(nums[i], i);
+        }
+        return null; // Return null if no solution is found
+    }
 }
---
+++ com/dev/basic/test/TwoSum_Problem_Test.java
@@ -0,0 +1,44 @@
+/*
+package com.dev.basic.test;
+
+import com.dev.basic.TwoSum_Problem;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TwoSum_Problem_Test {
+
+    @Test
+    void testTwoSumSuccess() {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = TwoSum_Problem.twoSum(nums, target);
+
+        assertNotNull(result, "Result should not be null");
+        assertEquals(0, result[0], "First index should be 0");
+        assertEquals(1, result[1], "Second index should be 1");
+    }
+
+    @Test
+    void testTwoSumFail() {
+        int[] nums = {1, 2, 3, 4};
+        int target = 10;
+
+        int[] result = TwoSum_Problem.twoSum(nums, target);
+
+        assertNull(result, "Result should be null when no solution exists");
+    }
+
+    @Test
+    void testTwoSumHappyPath() {
+        int[] nums = {3, 2, 4};
+        int target = 6;
+
+        int[] result = TwoSum_Problem.twoSum(nums, target);
+
+        assertNotNull(result, "Result should not be null");
+        assertEquals(1, result[0], "First index should be 1");
+        assertEquals(2, result[1], "Second index should be 2");
+    }
+}*/
